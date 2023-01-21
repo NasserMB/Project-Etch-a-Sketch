660eac102eaec500599f811630dfa7e2cf7b4c15
NasserMB <121682857+NasserMB@users.noreply.github.com>
2023-01-21 18:58:03 +0300

    Complete upto step 2 of the project

---
+++ script.js
@@ -0,0 +1,22 @@
+// Declerations
+let containerSize = 16;
+let container = document.createElement('div');
+
+// Container attributes
+container.className = 'container';
+
+// Itterate to create the grid
+for (let i = 0; i < containerSize; i++) {
+    let column = document.createElement('div');
+    column.className = 'column';
+
+    for (let j = 0; j < containerSize; j++){
+        let row = document.createElement('div');
+        row.className = 'row';
+        column.appendChild(row);
+    }
+    container.appendChild(column);
+}
+
+// Append container to the page
+document.body.appendChild(container);
