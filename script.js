58adf56447b58555d2992a20073134601c093a2a
NasserMB <121682857+NasserMB@users.noreply.github.com>
2023-01-25 21:13:19 +0300

    Complete Etch a Sketch project

--- script.js
+++ script.js
@@ -5,24 +5,49 @@ let container = document.createElement('div');
 // Container attributes
 container.className = 'container';
 
-// Itterate to create the grid
-for (let i = 0; i < containerSize; i++) {
-    let column = document.createElement('div');
-    column.className = 'column';
-
-    for (let j = 0; j < containerSize; j++){
-        let row = document.createElement('div');
-        row.className = 'row';
-        column.appendChild(row);
+// Clear grid function
+function clearGrid () {
+    let clearCells = document.getElementsByClassName('row');
+    for (let i = 0; i < clearCells.length; i++) {
+        clearCells[i].style.backgroundColor = '';
     }
-    container.appendChild(column);
 }
+
+// Resize grid
+function resizeGrid () {
+    containerSize = prompt('Enter grid size');
+    if (containerSize > 100) containerSize = 100;
+    removeGrid();
+    createGrid();
+    clearGrid();
+}
+
+// Remove grid
+function removeGrid () {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstElementChild);
+    }
+}
+
+// function to create the grid
+function createGrid () {
+    for (let i = 0; i < containerSize; i++) {
+        let column = document.createElement('div');
+        column.className = 'column';
+    
+        for (let j = 0; j < containerSize; j++){
+            let row = document.createElement('div');
+            row.className = 'row';
+            row.addEventListener('mouseover', () => {
+                row.style.backgroundColor = 'black';
+            });
+            column.appendChild(row);
+        }
+        container.appendChild(column);
+    }
+}
+
+createGrid();
 
 // Append container to the page
 document.body.appendChild(container);
-
-// ** WORKING ON IT **
-let cells = document.getElementsByClassName('row');
-container.addEventListener('mousedown', () => {
-    cells.classList.add('paint');
-});
